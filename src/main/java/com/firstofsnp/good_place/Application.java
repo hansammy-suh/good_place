1f8e115075672398b01201eaed87447f454358e8
Hansaem Suh <hansammy.suh@gmail.com>
2020-02-29 21:19:04 +0900

    Initial commit

---
+++ src/main/java/com/firstofsnp/good_place/Application.java
@@ -0,0 +1,13 @@
+package com.firstofsnp.good_place;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/firstofsnp/good_place/GoodPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstofsnp.good_place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
